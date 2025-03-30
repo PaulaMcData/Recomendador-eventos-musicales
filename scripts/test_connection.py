f58160fe08db02c41c3e4969d703c0fb2e3d264e
PaulaMcData <paulamc.data@gmail.com>
2025-03-30 18:41:37 +0200

    Test de conexion a MongoDB Atlas

---
+++ scripts/test_connection.py
@@ -0,0 +1,18 @@
+### Test connection VS y MongoDB ###
+
+from pymongo import MongoClient
+
+# Sustituir con mi cadena de conexion
+connection_string = "mongodb+srv://paulamcdata:Gp2025.@clustergp-eventos.c1syxjt.mongodb.net/?retryWrites=true&w=majority&appName=ClusterGP-Eventos"
+
+try:
+    # Conectar al cliente de MongoDB
+    client = MongoClient(connection_string)
+    # Obtener la lista de base de datos disponibles
+    dbs= client.list_database_names()
+
+    print("✅ Conexión exitosa a MongoDB Atlas")
+    print("📂 Bases de datos disponibles:", dbs)
+
+except Exception as e:
+    print("❌ Error de conexión:", e)
