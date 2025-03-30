e7db6e371b5eaac62bedb23dbcc4ed61f4078ca9
PaulaMcData <paulamc.data@gmail.com>
2025-03-31 01:17:05 +0200

    actualizacion de main

--- scripts/main.py
+++ scripts/main.py
@@ -6,7 +6,11 @@ def test_fetch_and_save():
     print("🔄 Iniciando la prueba de fetch y guardado de datos...")
     events = fetch_ticketmaster_data()
     if events:
-        save_events_to_mongodb(events)
+        inserted_ids = save_events_to_mongodb(events)
+        if inserted_ids:
+            print(f"✅ {len(inserted_ids)} eventos guardados correctamente en MongoDB.")
+        else:
+            print("❌ No se guardaron eventos en MongoDB.")
     else:
         print("❌ No se encontraron eventos musicales o hubo un error en la API.")
 
