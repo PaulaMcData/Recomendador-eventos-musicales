969062c5d6ff531acbaed6cc7437b983d2eeedf3
PaulaMcData <paulamc.data@gmail.com>
2025-04-01 00:23:59 +0200

    updated all

--- scripts/transform_event.py
+++ scripts/transform_event.py
@@ -12,15 +12,16 @@ def transform_event_data(event):
         "event_date": event.get("dates", {}).get("start", {}).get("localDate"),
         "event_time": event.get("dates", {}).get("start", {}).get("localTime"),
         "event_datetime": event.get("dates", {}).get("start", {}).get("dateTime"),
+        "segment": event.get("classifications", [{}])[0].get("segment", {}).get("name"),
         "genre": event.get("classifications", [{}])[0].get("genre", {}).get("name"),
         "subgenre": event.get("classifications", [{}])[0].get("subGenre", {}).get("name"),
-        "is_family_friendly": event.get("classifications", [{}])[0].get("family"),
+        "family_friendly": event.get("classifications", [{}])[0].get("family"),
         "event_description": event.get("info") or event.get("pleaseNote"),
         "event_type": event.get("type"),
         "event_image_url": event.get("images", [{}])[1].get("url"),
         "event_tags": [event.get("name") for t in event.get("classifications", [{}])[0].get("subGenre", {}).get("name", [])],
-        "start_date": event.get("dates", {}).get("start", {}).get("localDate"),
-        "end_date": event.get("dates", {}).get("end", {}).get("localDate"),
+        "start_date": event.get("sales", {}).get("public", {}).get("startDateTime"),
+        "end_date": event.get("sales", {}).get("public", {}).get("endDateTime"),
         "organizer": {"name": event.get("promoter", {}).get("name")},
         "ticket_status": event.get("dates", {}).get("status", {}).get("code"),
         "url_compra_entradas": event.get("url"),
@@ -31,15 +32,14 @@ def transform_event_data(event):
             }
             for ticket in event.get("priceRanges", [])
         ],
-        "currency": event.get("priceRanges", [{}])[0].get("currency"),
         "location": {
-            "city": event.get("place", {}).get("city", {}).get("name"),
-            "country": event.get("place", {}).get("country", {}).get("name"),
-            "latitude": event.get("place", {}).get("location", {}).get("latitude"),
-            "longitude": event.get("place", {}).get("location", {}).get("longitude"),
-            "postalCode": event.get("_embedded", {}).get("venues", [{}])[0].get("postalCode"),
-            "address": event.get("place", {}).get("address", {}).get("line1"),
-            "place": event.get("_embedded", {}).get("venues", [{}])[0].get("name")
+            "city": event.get("_embedded", {}).get("venues", {}).get("city"),
+            "country": event.get("_embedded", {}).get("venues", {}).get("country"),
+            "latitude": event.get("_embedded", {}).get("venues", {}).get("location", {}).get("latitude"),
+            "longitude": event.get("_embedded", {}).get("venues", {}).get("location", {}).get("longitude"),
+            "postalCode": event.get("_embedded", {}).get("venues", {}).get("postalCode"),
+            "address": event.get("_embedded", {}).get("venues", {}).get("address"),
+            "place": event.get("_embedded", {}).get("venues", {}).get("name")
         }     
     }
     
