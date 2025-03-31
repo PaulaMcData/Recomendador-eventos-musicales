0a5cb9b4bdba3da94d34fb8f9a031056d05d36bd
PaulaMcData <paulamc.data@gmail.com>
2025-03-31 23:11:00 +0200

    updated all

--- scripts/transform_event.py
+++ scripts/transform_event.py
@@ -5,12 +5,16 @@ def transform_event_data(event):
     transformed_event = {
         "id": event.get("id"),
         "name": event.get("name"),
-        "event_date": event.get("dates", {}).get("start", {}).get("localDate"),
-        "genre": event.get("classifications", [{}])[0].get("genre", {}).get("name"),
         "artist": event.get("_embedded", {}).get("attractions", [{}])[0].get("name"),
         "performers": [
             {"name": artist.get("name")} for artist in event.get("_embedded", {}).get("attractions", [])
         ],
+        "event_date": event.get("dates", {}).get("start", {}).get("localDate"),
+        "event_time": event.get("dates", {}).get("start", {}).get("localTime"),
+        "event_datetime": event.get("dates", {}).get("start", {}).get("dateTime"),
+        "genre": event.get("classifications", [{}])[0].get("genre", {}).get("name"),
+        "subgenre": event.get("classifications", [{}])[0].get("subGenre", {}).get("name"),
+        "is_family_friendly": event.get("classifications", [{}])[0].get("family"),
         "event_description": event.get("info") or event.get("pleaseNote"),
         "event_type": event.get("type"),
         "event_image_url": event.get("images", [{}])[0].get("url"),
