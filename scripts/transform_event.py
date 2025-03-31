bc049f42339a18bb2930dff7a7f1cdd9bfc77fca
PaulaMcData <paulamc.data@gmail.com>
2025-03-31 23:54:33 +0200

    updated all

--- scripts/transform_event.py
+++ scripts/transform_event.py
@@ -17,18 +17,13 @@ def transform_event_data(event):
         "is_family_friendly": event.get("classifications", [{}])[0].get("family"),
         "event_description": event.get("info") or event.get("pleaseNote"),
         "event_type": event.get("type"),
-        "event_image_url": event.get("images", [{}])[0].get("url"),
+        "event_image_url": event.get("images", [{}])[1].get("url"),
         "event_tags": [event.get("name") for t in event.get("classifications", [{}])[0].get("subGenre", {}).get("name", [])],
         "start_date": event.get("dates", {}).get("start", {}).get("localDate"),
         "end_date": event.get("dates", {}).get("end", {}).get("localDate"),
         "organizer": {"name": event.get("promoter", {}).get("name")},
-        "ticket_available": event.get("dates", {}).get("status", {}).get("code"),
+        "ticket_status": event.get("dates", {}).get("status", {}).get("code"),
         "url_compra_entradas": event.get("url"),
-        "dates": {
-            "status": {
-                "code": event.get("dates", {}).get("status", {}).get("code")
-            }
-        },
         "ticket_types": [
             {
                 "type": ticket.get("type"),
@@ -42,8 +37,9 @@ def transform_event_data(event):
             "country": event.get("place", {}).get("country", {}).get("name"),
             "latitude": event.get("place", {}).get("location", {}).get("latitude"),
             "longitude": event.get("place", {}).get("location", {}).get("longitude"),
+            "postalCode": event.get("_embedded", {}).get("venues", [{}])[0].get("postalCode"),
             "address": event.get("place", {}).get("address", {}).get("line1"),
-            "venue": event.get("_embedded", {}).get("venues", [{}])[0].get("name")
+            "place": event.get("_embedded", {}).get("venues", [{}])[0].get("name")
         }     
     }
     
